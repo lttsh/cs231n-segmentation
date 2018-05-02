10eaf5b0b60d1f13ebcd823965ccb929b887deea
lshao <laetitia.shao@gmail.com>
2018-05-02 16:31:21 -0700

    Fix setup script

--- setup.sh
+++ setup.sh
@@ -1,21 +1,19 @@
-## Download COCO-Stuff API and install Python API
-## Not sure if needed/ how to use yet.
-# wget https://github.com/nightrome/cocoapi/archive/master.zip
-# unzip master.zip
-# rm master.zip
-# mv cocoapi-master/ coco/
-# cd coco/PythonAPI && make && cd ../..
+# Download COCO-Stuff API and build PythonAPI
+wget https://github.com/nightrome/cocoapi/archive/master.zip
+unzip master.zip
+rm master.zip
+mv cocoapi-master/ cocostuff/
+cd cocostuff/PythonAPI && make && cd ../..
 
-## Downloqd COCO-Stuff dataset and baseline model
+## Download COCO-Stuff dataset and baseline model
 wget https://github.com/nightrome/cocostuff/archive/master.zip
 unzip master.zip
 rm master.zip
-mv cocostuff-master/ cocostuff/
 cd cocostuff
 
 # Download everything
 # Images
-# wget --directory-prefix=downloads http://images.cocodataset.org/zips/train2017.zip
+wget --directory-prefix=downloads http://images.cocodataset.org/zips/train2017.zip
 wget --directory-prefix=downloads http://images.cocodataset.org/zips/val2017.zip
 
 # PNG annotations
@@ -26,12 +24,17 @@ wget --directory-prefix=downloads http://calvin.inf.ed.ac.uk/wp-content/uploads/
 wget --directory-prefix=downloads http://images.cocodataset.org/annotations/annotations_trainval2017.zip
 
 # Unpack everything
-mkdir -p dataset/images
-mkdir -p dataset/annotations
+# mkdir -p images
+# mkdir -p annotations
 
-# unzip downloads/train2017.zip -d dataset/images/
-unzip downloads/val2017.zip -d dataset/images/
-unzip downloads/stuffthingmaps_trainval2017.zip -d dataset/annotations/
-unzip downloads/stuff_trainval2017.zip -d dataset/annotations/
-unzip downloads/annotations_trainval2017.zip -d dataset/annotations/
+unzip downloads/train2017.zip -d images/
+unzip downloads/val2017.zip -d images/
+unzip downloads/stuffthingmaps_trainval2017.zip -d annotations/
+unzip downloads/stuff_trainval2017.zip -d annotations/
+unzip downloads/annotations_trainval2017.zip -d annotations/
+
+mv annotations/annotations/* annotations/
+rm -rf annotations/annotations
+rm -rf LuaAPI/
+rm -rf MatlabAPI/
 cd ..
