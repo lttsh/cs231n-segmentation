f329882eda389fc911ae65bb195e1be7d04797aa
sudarshanseshadri <sudarshadri@gmail.com>
2018-05-12 15:21:10 -0700

    Need accuracy function and model

--- code/train.py
+++ code/train.py
@@ -1,13 +1,21 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
+from torch.utils.data import DataLoader
 import CocoStuffDataSet
 
-train_loader = CocoStuffDataSet(train_set, batch_size=32, shuffle=True)
-test_loader = CocoStuffDataSet(test_set, batch_size=32, shuffle=False)
+train_loader = DataLoader(CocoStuffDataSet(mode='train'), batch_size=32, shuffle=True)
+val_loader = DataLoader(CocoStuffDataSet(mode='val'), batch_size=32, shuffle=False)
 
 class Trainer():
     def __init__(self, net, train_loader, val_loader):
+        """
+        Training class for a specified model
+        Args:
+            net: (model) model to train
+            train_loader: (DataLoader) train data
+            val_load: (DataLoader) validation data
+        """
         self._net = net
         self._train_loader = train_loader
         self._val_loader = val_loader
@@ -15,6 +23,16 @@ class Trainer():
         self._optimizer = optim.Adam(self._net.parameters())
 
     def _train_batch(self, mini_batch_data, mini_batch_labels):
+        """
+        Performs one gradient step on a minibatch of data
+        Args:
+            mini_batch_data: (torch.Tensor) shape (N, C_in, H, W) 
+                where self._net operates on (C_in, H, W) dimensional images
+            mini_batch_labels: (torch.Tensor) shape (N, C_out, H, W)
+                a batch of (H, W) binary masks for each of C_out classes
+        Return:
+            loss: (float) loss computed by self._criterion on input minibatch
+        """
         self._optimizer.zero_grad()
         out = self._net(batch_data)
         loss = self._criterion(out, batch_labels)
@@ -23,13 +41,25 @@ class Trainer():
 
         return loss
 
-    def train(self, num_epochs):
+    def train(self, num_epochs, print_every=100):
+        """
+        Trains the model for a specified number of epochs
+        Args:
+            num_epochs: (int) number of epochs to train
+            print_every: (int) number of minibatches to process before
+                printing loss. default=100
+        """
+        print_i = 0
         for epoch in range(num_epochs):
             print "Starting epoch {}".format(epoch)
             for mini_batch_data, mini_batch_labels in self._train_loader:
-                self._train_batch(mini_batch_data, mini_batch_labels)
+                loss = self._train_batch(mini_batch_data, mini_batch_labels)
+                if print_i % print_every == 0:
+                    print("Loss: {}".format(loss))
+                print_i += 1
 
-            for batch_data, batch_labels in sef._val_loader
+            # for batch_data, batch_labels in sef._val_loader:
+
 
 
 
